1897524700b335648a90889136eb7bc2040590c9
Patryk Palej <patrykpalej@gmail.com>
2024-05-20 10:32:35 +0200

    2.2 [Z] Ścieżki i routing

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ tasks_data = [
     {"description": "Do exercises", "priority": 2, "is_complete": False},
 ]
 
+users_data = [
+    {"username": "Andrzej", "password": "qwerty123", "is_admin": True},
+    {"username": "Andżela", "password": "hasło1!", "is_admin": False}
+]
+
 
 @app.get("/")
 def root():
@@ -17,3 +22,8 @@ def root():
 @app.get("/tasks")
 def get_tasks():
     return {"result": tasks_data}
+
+
+@app.get("/users")
+def get_users():
+    return {"result": users_data}
