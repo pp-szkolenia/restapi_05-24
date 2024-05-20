fb913402d13428c2c4b6f67b7431a6f1c37db35a
Patryk Palej <patrykpalej@gmail.com>
2024-05-20 09:59:35 +0200

    2.2 [W] Ścieżki i routing

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+tasks_data = [
+    {"description": "Learn FastAPI", "priority": 3, "is_complete": True},
+    {"description": "Do exercises", "priority": 2, "is_complete": False},
+]
+
 
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
+
+
+@app.get("/tasks")
+def get_tasks():
+    return {"result": tasks_data}
