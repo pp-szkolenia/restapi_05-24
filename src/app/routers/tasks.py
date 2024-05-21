04dd2233da36fcecd6b94d20048b302a5356e0fd
Patryk Palej <patrykpalej@gmail.com>
2024-05-21 11:33:38 +0200

    4.2 [W] Implementacja operacji bazodanowych w psycopg2

--- src/app/routers/tasks.py
+++ src/app/routers/tasks.py
@@ -4,24 +4,35 @@ import random
 
 from app.utils import get_item_by_id, get_item_index_by_id
 from app.models import TaskBody
+from db.utils import connect_to_db
 
 
 router = APIRouter()
 
-tasks_data = [
-    {"id": 1, "description": "Learn FastAPI", "priority": 3, "is_complete": True},
-    {"id": 2, "description": "Do exercises", "priority": 2, "is_complete": False},
-]
-
 
 @router.get("/tasks", tags=["tasks"])
 def get_tasks():
+    conn, cursor = connect_to_db()
+
+    cursor.execute("SELECT * FROM tasks")
+    tasks_data = cursor.fetchall()
+
+    conn.close()
+    cursor.close()
+
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={"result": tasks_data})
 
 
 @router.get("/tasks/{id_}", tags=["tasks"])
 def get_task_by_id(id_: int):
-    target_task = get_item_by_id(tasks_data, id_)
+    conn, cursor = connect_to_db()
+
+    cursor.execute("SELECT * FROM tasks WHERE id = %s", (id_,))
+    target_task = cursor.fetchone()
+
+    conn.close()
+    cursor.close()
+
     if target_task is None:
         message = {"error": f"Task with id {id_} does not exist!"}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
@@ -31,38 +42,60 @@ def get_task_by_id(id_: int):
 
 @router.post("/tasks", status_code=status.HTTP_201_CREATED, tags=["tasks"])
 def create_task(body: TaskBody):
-    new_task = body.model_dump()
-    random_id = random.randint(1, 10000)
-    new_task["id"] = random_id
+    conn, cursor = connect_to_db()
 
-    tasks_data.append(new_task)
+    insert_query_template = f"""INSERT INTO tasks (description, priority, is_complete)
+                                VALUES (%s, %s, %s) RETURNING *"""
+    insert_query_values = (body.description, body.priority, body.is_complete)
+
+    cursor.execute(insert_query_template, insert_query_values)
+    new_task = cursor.fetchone()
+    conn.commit()
+
+    conn.close()
+    cursor.close()
 
     return {"message": "New task added", "details": new_task}
 
 
 @router.delete("/tasks/{id_}", tags=["tasks"])
 def delete_task_by_id(id_: int):
-    target_index = get_item_index_by_id(tasks_data, id_)
+    conn, cursor = connect_to_db()
 
-    if target_index is None:
+    delete_query = "DELETE FROM tasks WHERE id = %s RETURNING *"
+
+    cursor.execute(delete_query, (id_,))
+    deleted_task = cursor.fetchone()
+    conn.commit()
+
+    conn.close()
+    cursor.close()
+
+    if not deleted_task:
         message = {"error": f"Task with id {id_} does not exist"}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
 
-    tasks_data.pop(target_index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @router.put("/tasks/{id_}", tags=["tasks"])
 def update_task_by_id(id_: int, body: TaskBody):
-    target_index = get_item_index_by_id(tasks_data, id_)
+    conn, cursor = connect_to_db()
 
-    if target_index is None:
+    update_query_template = """UPDATE tasks SET description = %s, priority = %s, is_complete = %s
+                               WHERE id = %s RETURNING *"""
+    update_query_values = (body.description, body.priority, body.is_complete, id_)
+
+    cursor.execute(update_query_template, update_query_values)
+    updated_task = cursor.fetchone()
+    conn.commit()
+
+    conn.close()
+    cursor.close()
+
+    if not updated_task:
         message = {"error": f"Task with id {id_} does not exist"}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
-
-    updated_task = body.model_dump()
-    updated_task["id"] = id_
-    tasks_data[target_index] = updated_task
 
     message = {"message": f"Task with id {id_} updated", "new_value": updated_task}
     return JSONResponse(status_code=status.HTTP_200_OK, content=message)
